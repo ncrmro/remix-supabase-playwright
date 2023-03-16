26c3ad21e46df3b2c65f564c8660f0f55c0c7cae
Nicholas Romero <ncrmro@gmail.com>
2023-03-15 20:49:16 -0500

    feat(ci): (#1)
    * feat(ci): supabase linting
    
    * fix(db): seed
    
    * fix(db): seed
    
    * feat(ci): initial build command
    
    * feat(ci): playwright
    
    * feat(ci): playwright
    
    * feat(ci): set timeout
    
    * fix(ci): start full supabase

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import * as process from "process";
 
 /**
  * Read environment variables from file.
@@ -33,9 +34,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
+    actionTimeout: 3000,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:4000",
+    baseURL: "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -83,8 +84,9 @@ export default defineConfig({
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: "yarn start",
+    url: "http://localhost:3000",
+    reuseExistingServer: true,
+  },
 });
